444d10814ec0898a2611ef92771f2b7dfab75582
elvis <elvisfernando9@hotmail.com>
2022-03-20 05:55:38 -0500

    Usar Index en vez de App

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Index from "./pages/Index";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Index/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/Index.tsx
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+interface Props {
+}
+
+interface State {
+
+}
+
+class Index extends Component<Props, State> {
+    render() {
+        return (<main>
+            I am index
+        </main>);
+    }
+}
+
+export default Index;
