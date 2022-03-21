467b98bf3f749ec33820b16cbde1fec7b19e7e62
elvis <elvisfernando9@hotmail.com>
2022-03-20 21:09:19 -0500

    Validar argumento en onSearch

--- src/components/Search.test.tsx
+++ src/components/Search.test.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import Search from './Search';
 
-const setup = () => {
-  const utils = render(<Search />)
-  const input = utils.getByPlaceholderText('Buscar...')
-  return {
-    input,
-    ...utils,
-  }
-}
+test('Comprobar que funciona el prop onSearch', () => {
+  const onSearch = jest.fn();
+  render(<Search onSearch={onSearch}/>);
 
+  const input = screen.getByPlaceholderText('Buscar...')
 
-test('Comprobar evento onSearch', () => {
-  const {input} = setup()
   fireEvent.change(input, {target: {value: 'Pikachu'}})
   // @ts-ignore
   expect(input.value).toBe('Pikachu')
+  expect(onSearch).toHaveBeenCalledWith('pikachu');
 });
